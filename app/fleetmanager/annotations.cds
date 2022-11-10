f8806c9c47107541f1355284883d9bb4c8f267fd
Ken De Crop <ken.decrop@amista.be>
2022-11-10 08:22:49 +0000

    added sapui5 to the mix

---
+++ app/fleetmanager/annotations.cds
@@ -0,0 +1,1 @@
+using FleetService as service from '../../srv/service';
---
+++ srv/service.js
@@ -0,0 +1,7 @@
+module.exports = (srv) => {
+
+    //Here we auto generate an ID and do some checks on 
+    srv.before("INSERT", 'Vehicles', async (req) => {
+
+    });
+}
