789276f26d36c9e8c45027bca53a98e94cfb3bc0
Ken De Crop <ken.decrop@amista.be>
2022-12-14 10:03:49 +0000

    changed service for logging purposes

--- srv/service.js
+++ srv/service.js
@@ -1,21 +1,25 @@
 const sequenceHelperForVehicles = require("./Helpers/sequenceHelperForVehicles");
+const logger = require("./Helpers/logger");
 const cds = require('@sap/cds');
 
 module.exports = (srv) => {
 
     //Here we auto generate an ID and do some checks on 
     srv.before("CREATE", 'Vehicles', async (req) => {
-        req.data.ID = await sequenceHelperForVehicles.getNewID("SEQ_VEHICLE");
+        console.log(req);
+        debugger;
 
         if (req.data.VIN_NUMBER == "") {
             req.reject(400, "VIN_NUMBER is required");
         }
+
         console.log(req);
         let query = SELECT`VIN_NUMBER`.from`Vehicles`.where`VIN_NUMBER = ${req.data.VIN_NUMBER}`;
         let vinInDb = await srv.run(query);
-            if(vinInDb.length > 0) {
-                req.reject(400, "VIN_NUMBER is known in database. Only vehicles with unique vin number can be added");
-            }
+
+        if (vinInDb.length > 0) {
+            req.reject(400, "VIN_NUMBER is known in database. Only vehicles with unique vin number can be added");
+        }
 
         if (req.data.MODEL == "") {
             req.reject(400, "MODEL is required");
@@ -23,5 +27,8 @@ module.exports = (srv) => {
         if (req.data.BRAND == "") {
             req.reject(400, "BRAND is required");
         }
+
+        req.data.ID = await sequenceHelperForVehicles.getNewID("SEQ_VEHICLE");
+        logger.writeInfo("db", "Ken", "Vehicle", "nothing", req.data.VIN_NUMBER, req.data.ID);
     });
 }
